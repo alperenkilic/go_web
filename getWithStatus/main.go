06ad59025ef256435662ae536e15e3f27247177c
alperenkilic <kilicalperen@icloud.com>
2021-06-12 00:56:06 +0300

    edited modules

--- getWithStatus/main.go
+++ getWithStatus/main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	app := fiber.New()
 	app.Get("/lookstatus", func(c *fiber.Ctx) error {
-		return c.Status(fiber.StatusBadRequest).Send([]byte("hello but look at the status "))
+		return c.Status(fiber.StatusLocked).Send([]byte("hello but pasta"))
 		// google developer toolkit -> network -> all -> Summary
 	})
 	app.Listen(":3000")
--- serveHtml/go.mod
+++ serveHtml/go.mod
@@ -2,4 +2,9 @@ module github.com/alperenkilic/go_web/serveHtml
 
 go 1.16
 
-require github.com/gofiber/fiber/v2 v2.12.0
+require (
+	github.com/andybalholm/brotli v1.0.3 // indirect
+	github.com/gofiber/fiber/v2 v2.12.0
+	github.com/klauspost/compress v1.13.0 // indirect
+	golang.org/x/sys v0.0.0-20210611083646-a4fc73990273 // indirect
+)
